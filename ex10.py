b7a79d4be7634575b09bd3ef999c3db4fd6059d3
AyeAyeZin <aaz.ayeayezin.123456@gmail.com>
2018-06-07 01:25:31 +0630

    Add

--- ex10.py
+++ ex10.py
@@ -1,8 +1,7 @@
-tabby_cat="\tI'm tabbed in."
-persian_cat="I'm split\non a line"
-
-backlash_cat="I'm \\ a \\ cat."
-fat_cat="""
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+fat_cat = """
 I'll do a list:
     \t* Cat food
     \t* Fishies
@@ -10,5 +9,5 @@ I'll do a list:
     """
     print(tabby_cat)
     print(persian_cat)
-    print(backslah_cat)
+    print(backslash_cat)
     print(fat_cat)
