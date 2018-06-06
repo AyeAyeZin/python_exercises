39e4c17109d63fb7c602c0c07002df27c7bf8a2f
AyeAyeZin <aaz.ayeayezin.123456@gmail.com>
2018-06-06 23:15:12 +0630

    ADD

--- ex4.py
+++ ex4.py
@@ -6,4 +6,5 @@ car_not_driven=cars-drivers
 cars_driven=drivers
 print("There are", cars,"cars availble")
 print("There are only",drivers,"drivers availble")
-print("There will be",cars_not_driven,"empty cars today")
+print("There will be",car_not_driven,"empty cars today")
+print("There are",cars_in_space,"space availble in car")
