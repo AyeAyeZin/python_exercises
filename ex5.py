31113306f2127c740d9326f4e2538e46262e241d
AyeAyeZin <aaz.ayeayezin.123456@gmail.com>
2018-06-06 23:29:04 +0630

    ADD

---
+++ ex5.py
@@ -0,0 +1,15 @@
+my_name='Aye Aye Zin'
+my_age=22
+my_height=65#inches
+my_weiight=120#lbs
+my_eyes='Black'
+my_teeth='white'
+my_hair='Brown'
+print("Let's talk about {my_name}")
+print("She's {my_height} inches tall")
+print("She's {my_weight} pounds heavy")
+print("Actally that's not too heavy")
+print("She's got {my_eyes} eyes and {my_hair} hair")
+print("Her teeth are usually {my_teeth} White depending on the coffee")
+total=my_age+my_height+my_weight
+        print("If I add {my_age}, {my_height} and {my_weight} I get {total}")
