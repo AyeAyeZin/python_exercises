8c586421aaddca303aa29d3df5af9edfcb14fe9b
AyeAyeZin <aaz.ayeayezin.123456@gmail.com>
2018-06-07 00:31:19 +0630

    Add

--- ex8.py
+++ ex8.py
@@ -2,5 +2,5 @@ formatter="{} {} {} {}"
 print(formatter.format(1,2,3,4))
 print(formatter.format("one", "two", "three", "four"))
 print(formatter.format(True, False, False, True))
-print(formatter.format(formatter, formatter))
+print(formatter.format(formatter, formatter, formatter, formatter))
 print(formatter.format("try your", "Own text here", "Maybe a poem", "Or a song dabout fear"))
