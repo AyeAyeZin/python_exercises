14d224e83c64fefe91617d4f11a714c4c8d8fb85
AyeAyeZin <aaz.ayeayezin.123456@gmail.com>
2018-06-07 01:33:46 +0630

    Add

--- ex10.py
+++ ex10.py
@@ -1,12 +1,14 @@
 tabby-cat="\tI'm tabbed in."
 persian-cat="I'm split\non a line."
 backslash-cat="I'm \\ a \\ cat."
+
 fat-cat="""
 I'll do a list:
     \t* Cat food
     \t* Fishies
     \t* Catnip\n\t* Grass
     """
+
     print(tabby-cat)
     print(persian-cat)
     print(backslash-cat)
