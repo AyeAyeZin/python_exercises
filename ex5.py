b1ec8025d189e5bc112af6c901e4dcd77c5d0741
AyeAyeZin <aaz.ayeayezin.123456@gmail.com>
2018-06-06 23:35:50 +0630

    ADD

--- ex5.py
+++ ex5.py
@@ -3,13 +3,12 @@ my_age=22
 my_height=65#inches
 my_weiight=120#lbs
 my_eyes='Black'
-my_teeth='white'
 my_hair='Brown'
-print(f"Let's talk about {my_name}")
-print(f"She's {my_height} inches tall")
-print(f"She's {my_weight} pounds heavy")
-print(f"Actally that's not too heavy")
-print(f"She's got {my_eyes} eyes and {my_hair} hair")
-print(f"Her teeth are usually {my_teeth} White depending on the coffee")
+print(f"Let's talk about {my_name}.")
+print(f"She's {my_height} inches tall.")
+print(f"She's {my_weight} pounds heavy.")
+print("Actally that's not too heavy.")
+print(f"She's got {my_eyes} eyes and {my_hair} hair.")
+print(f"Her teeth are usually {my_teeth} White depending on the coffee.")
 total=my_age+my_height+my_weight
-        print(f"If I add {my_age}, {my_height} and {my_weight} I get {total}")
+        print(f"If I add {my_age}, {my_height} and {my_weight} I get {total}.")
