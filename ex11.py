b2d7bfacdfa084e9c7bac28603b75c5173612740
AyeAyeZin <aaz.ayeayezin.123456@gmail.com>
2018-06-07 01:48:23 +0630

    Add

--- ex11.py
+++ ex11.py
@@ -1,6 +1,6 @@
 print("How old are you?", end=' ')
-age=input()
-print("How tall are you?", end=' ')
+age=input(38)
+print("how tall are you?", end=' ')
 height=input()
 print("How much do you weight?", end=' ')
 weight= input()
