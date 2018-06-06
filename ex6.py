b7268607b24b9618d6c3e4d0f0d6574bb242b4b0
AyeAyeZin <aaz.ayeayezin.123456@gmail.com>
2018-06-07 00:03:56 +0630

    Add

--- ex6.py
+++ ex6.py
@@ -6,7 +6,7 @@ y=f"Those who know {binary} and those who {do_not}."
 print(x)
 print(y)
 print(f"I said:{x}")
-print(f"I also said:'{y'}")
+print(f"I also said:'{y}'")
 hilarious=False
 joke_evaluation="Isn't that joke so funny?!{}"
 print(joke_evaluation.format(hilarious))
