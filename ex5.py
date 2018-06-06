6c0049ec4e2a8df3a40174c41627dcc27fe7740c
AyeAyeZin <aaz.ayeayezin.123456@gmail.com>
2018-06-06 23:54:01 +0630

    Add

--- ex5.py
+++ ex5.py
@@ -12,6 +12,6 @@ print("Actally that's not too heavy.")
 print(f"She's got {my_eyes} eyes and {my_hair} hair.")
 print(f"Her teeth are usually {my_teeth} White depending on the coffee.")
 total=my_age+my_weight+my_height
-print(f"If I add {my_age}, {my_heught}, and {my_weight} I get {total}")
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}")
 
 
