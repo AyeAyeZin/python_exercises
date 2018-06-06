4b59d974299830ee0394ef3e84cd80f68cf854db
AyeAyeZin <aaz.ayeayezin.123456@gmail.com>
2018-06-07 01:58:16 +0630

    Add

--- ex12.py
+++ ex12.py
@@ -1,4 +1,4 @@
-age=input("How old are you?")
-height=input("How tall are you?")
-weight=input("How much do you weight?")
+age=input("How old are you? 38")
+height=input("How tall are you? `120")
+weight=input("How much do you weight? 129")
 print(f"So, you're {age} old, {height} tall, and {weight} heavy.")
