024ed0a0262327c81b8557b355fa4bcf11cac10f
AyeAyeZin <aaz.ayeayezin.123456@gmail.com>
2018-06-07 20:35:21 +0630

    Add

---
+++ ex18.py
@@ -0,0 +1,11 @@
+def print_two(*args):
+arg1, arg2=args
+print(f"arg1: {aarg1}, arg2: {arg2}")
+def print_two again(arg1,arg2):
+    print(f"arg1: {arg1}, arg2: {arg2}")
+    def print_one(arg1):
+        print("I got nothin'.")
+        print_two("Zed","Shaw")
+        print_two_again("Zed"."Shaw")
+        print_one("First")
+        
