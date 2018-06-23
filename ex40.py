e0fdabd5eb2817fb2f996c7acb49799b6939bd79
AyeAyeZin <aaz.ayeayezin.123456@gmail.com>
2018-06-23 22:15:13 +0630

    Add

--- ex40.py
+++ ex40.py
@@ -1,14 +1,28 @@
-class Song(object):
-    def _init_(self,lyrics):
-        self.lyrics=lyrics
-        def sing_me_a_song(self):
-            for line in self.lyrics:
-                print(line)
-happy_bady=Song(["Happy birthday to you",
-                "I don't want to get sued",
-                "So I'll stop right there"])
-bulls_on_parade=Song(["They rally around that family",
-                    "With pockets full of shells"])
-happy_bady.sing_me_a_song()
-bulls_on_parade.sing_me_a_song()
+mystuff={'apple':"I AM APPLES"}
+print(mystuff['apple'])
 
+def apple():
+    print("I AM APPLES!")
+tangerine="Living reflection of a dream"
+
+mystuff['apple']
+class Mystuff(object):
+    def _init_(self):
+        self.tangerine="And now a thousand years between"
+    def apple(self):
+        print("I AM CLASSY APPLES!")
+thing=Mystuff()
+thing.apple()
+class Song(object):
+    def _init_(self, lyrics):
+        self.lyrics=lyrics
+    def sing_me_a_song(self):
+        for line in self.lyrics:
+            print(line)
+happy_bday=Song(["Happy birthday to you",
+                 "I don't want to get sued",
+                 "So I'll stop right there"])
+bulls_on_parade=Song(["They rally around the family",
+                      "With pockets full o shells"])
+happy_bday.sing_me_a_song()
+bulls_on_parade.sing_me_a_song()
