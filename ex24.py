4efff74fbcdb494ce55797f54e90abbf7b266e54
AyeAyeZin <aaz.ayeayezin.123456@gmail.com>
2018-06-23 23:47:28 +0630

    Add

--- ex24.py
+++ ex24.py
@@ -1,29 +1,30 @@
-print("Let's practice everything.")
+print("Let's practice everything")
 print('You\'d need to know \'bout escapes with \\ that do:')
-print('\n newlines and \t tabs.')
+print('\n newliness and \t tabs.')
 poem="""
 \tThe lovely world
-with logic so formly planted
+with logic so planted
 cannot discern \n the needs of love
-nor complehend passion from intuition
+nor comprehend passion from intuition
 and requires an explanation
-\n\twhere there is none
+\n\twhere there is none.
 """
-print(".........")
+print("...............")
 print(poem)
-print(".........")
+print("...............")
 five=10-2+3-6
-print(f"This should be fives: {five}")
+print(f"This should be five:{five}")
 def secret_formula(started):
-    jelly_beans=started*500
-    jars=jelly_beans/100
+    jelly_beans=started+500
+    jars=jelly_beans/1000
     crates=jars/100
     return jelly_beans,jars,crates
 start_point=1000
-beans,jars,crates=secret_formula(start_point)
-print("With a starting point of:{}".format(start_point))
+beans, jars, crates=secret_formula(start_point)
+print("With a starting point of: {}".format(start_point))
 print(f"We'd have {beans} beans, {jars} jars, and {crates} crates.")
 start_point=start_point/10
 print("We can also do that this way:")
 formula=secret_formula(start_point)
-print("We'd have {} beans, {} jars, and crates.".format(*formula))
+print("we'd have {} beans, {} jars, and {} crates.".format(*formula))
+
