c6162d9f8d86cb0103edddd237f7341e96f40943
AyeAyeZin <aaz.ayeayezin.123456@gmail.com>
2018-06-24 09:27:53 +0630

    Add

--- ex41.py
+++ ex41.py
@@ -16,41 +16,44 @@ PHARASES={
         "from *** get the *** function, call it with params self, @@@.",
         "***.***='***'":
         "From *** get the *** attribute and set it to '***'."
-        if len(sys.argv)==2 and sys.argv[1]=="english":
+}
+if len(sys.argv)==2 and sys.argv[1]=="english":
         PHRASE_FIRST=True
-        else:
+else:
         PHRASE_FIRST=False
         for word in urlopen(WORD_URL).readline():
-        WORDS.append(str(word.strip(),encoding="utf-8"))
+                WORDS.append(str(word.strip(),encoding="utf-8"))
         def convert(snippet, phrase):
-        class_names=[w.capitalize() for w in
-            randoms.sample(WORDS, snippet.count("%%%"))]
+                class_names=[w.capitalize() for w in
+            random.sample(WORDS, snippet.count("%%%"))]
         other_names=random.sample(WORDS,snippet.count("***"))
         results=[]
-        param_name=[]
+        param_names=[]
         for i in range(0,snippet.count("@@@")):
-        param_count=random.randint(1,3)
+                param_count=random.randint(1,3)
         param_names.append(', '.join(
             random.sample(WORDS,param_count)))
         for sentence in snippet, phrase:
-        result=sentence[:]
-        for word in class_names:
-        result=result.replace("%%%", word, 1)
-        for word in other_names:
-        result=result.replace("***", word, 1)
-        for word in param_names:
-        result=result.replace("@@@", word, 1)
-        results.append(result)
-        return results
-        try:
+                result=sentence[:]
+                for word in class_names:
+                        result=result.replace("%%%", word, 1)
+                for word in other_names:
+                        result=result.replace("***", word, 1)
+                for word in param_names:
+                        result=result.replace("@@@", word, 1)
+                        results.append(result)
+                return results
+try:
         while True:
-        snippets=list(PHRASES.keys())
-        random.shuffle(snippets)
-        for snippet in snippets:
-        phrase=PHRASES[snippet]
-        question, answer=answer,question
-        print(question)
-        input("> ")
+                snippets=list(PHRASES.keys())
+                random.shuffle(snippets)
+                for snippet in snippets:
+                        phrase=PHRASES[snippet]
+                question, answer=answer,question
+                if PHRASE_FIRST:
+                        question,answer=answer,question
+                print(question)
+                input("> ")
         print(f"ANSWER: {answer}\n\n")
-        except EOFError:
+except EOFError:
         print("\nBye")
