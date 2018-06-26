9696ba0518174c2fbac5f2321c88c828fe7bcde3
AyeAyeZin <39287987+AyeAyeZin@users.noreply.github.com>
2018-06-26 20:18:44 +0630

    Update ex101.py
    Change cat to rat

--- ex101.py
+++ ex101.py
@@ -1,13 +1,13 @@
-tabby_cat="\t I'm tabbed in"
-persian_cat="I'm slip \n on a line"
-backslash_cat="I'm \\ a \\ cat"
-fat_cat="""
+tabby_rat="\t I'm tabbed in"
+persian_rat="I'm slip \n on a line"
+backslash_rat="I'm \\ a \\ cat"
+fat_rat="""
 I'll do a list
 \t* Cat food
 \t* Fishes
 \t* Catnip\n\t* Grass
 """
-print(tabby_cat)
-print(persian_cat)
-print(backslash_cat)
-print(fat_cat)
+print(tabby_rat)
+print(persian_rat)
+print(backslash_rat)
+print(fat_rat)
